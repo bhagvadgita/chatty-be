dc0b366a2990c47c4afcaeab5b27a02ed869a4c0
Abhay Saxena <abhaysaxena3111@gmail.com>
2025-05-12 10:41:20 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://admin:rs7oi7zFVVH8wIVl@free.n9yphfs.mongodb.net/chat");
+    const conn = await mongoose.connect("");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
